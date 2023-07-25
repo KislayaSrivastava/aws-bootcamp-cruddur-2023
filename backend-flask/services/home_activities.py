c9683f90c0dcf0a7ef07a7ddf7c0980980efa601
Kislaya Srivastava <Kislaya.srivastava@gmail.com>
2023-07-25 05:19:39 +0000

    Updated PSQL Driver

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool
+from lib.db import pool,query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
@@ -13,21 +13,8 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
-      sql = ("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
+      sql = query_wrap_array("""
+      SELECT * FROM ACTIVITIES
       """)
       print(sql)
       with pool.connection() as conn:
@@ -36,4 +23,5 @@ class HomeActivities:
           # this will return a tuple
           # the first field being the data
           json = cur.fetchone()
+      print(json[0])
       return json[0]
