abeffb2e36be078cb7af594c1da67ca4f911a558
Kislaya Srivastava <Kislaya.srivastava@gmail.com>
2023-07-26 07:53:16 +0000

    Changes for Production database

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,21 +1,12 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Kislaya Srivastava','kislaya.srivastava@gmail.com','Kislaya','8143cd9a-70f1-70fc-9531-4ee18e32a4c8');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'Kislaya' LIMIT 1),
     'This message was imported as seed data into Production DATABASE!',
-    current_timestamp + interval '10 day'
-  );
-
-INSERT INTO public.activities (user_uuid, message, expires_at)
-VALUES
-  (
-    (SELECT uuid from public.users WHERE users.handle = 'MksBlr' LIMIT 1),
-    'This message was written to Production database and imported as seed data!',
     current_timestamp + interval '10 day'
   );
